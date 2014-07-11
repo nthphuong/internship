e6834c5c087d4d5ad76a4e6e4d4fd41312b92ed6
Dang Vinh <mr.ngodangvinh@gmail.com>
2014-07-11 13:44:58 +0700

    no message

--- index.php
+++ index.php
@@ -1,11 +1,7 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: GoD.FlamingAIR
- * Date: 7/11/14
- * Time: 9:41 AM
- */
-?>
+    <?php
+
+
+    ?>
 
 <html lang="en">
 <head>
@@ -14,8 +10,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
     <title>REQUESTING MAIL</title>
     <meta name="author" content="GoD.DKVA">
-    <link rel="stylesheet" type="text/css" media="all" href="style.css">
-    <link rel="stylesheet" type="text/css" media="all" href="responsive.css">
+    <link rel="stylesheet" type="text/css" media="all" href="css/style.css">
+    <link rel="stylesheet" type="text/css" media="all" href="css/responsive.css">
 </head>
 
 <body>
@@ -25,25 +21,23 @@
     <form name="form" id="test-form" method="post" action="#">
         <div id="wrapping" class="clearfix">
             <section id="aligned">
-                <input type="text" name="userID" id="userID" placeholder="UID" autocomplete="off" tabindex="1" class="txtinput">
+                <input type="text" name="uid" id="uid" placeholder="Your ID" autocomplete="off" tabindex="1" class="txtinput" required>
 
-                <input type="text" name="name" id="name" placeholder="Your name" autocomplete="off" tabindex="2" class="txtinput">
+                <input type="text" name="name" id="name" placeholder="Your name" autocomplete="off" tabindex="2" class="txtinput" required>
 
-                <input type="email" name="email" id="email" placeholder="Your e-mail address" autocomplete="off" tabindex="3" class="txtinput">
+                <input type="email" name="email" id="email" placeholder="Your e-mail address" autocomplete="off" tabindex="3" class="txtinput" required>
 
-                <input type="url" name="website" id="website" placeholder="Your address" autocomplete="off" tabindex="4" class="txtinput">
+                <input type="text" name="address" id="address" placeholder="Your address" autocomplete="off" tabindex="4" class="txtinput">
 
             </section>
 
             <section id="aside" class="clearfix">
                 <section id="recipientcase">
                     <h3>Recipient:</h3>
-                    <select id="recipient" name="recipient" tabindex="6" class="selmenu">
-                        <option value="staff">Site Staff</option>
-                        <option value="editor">Editor-in-Chief</option>
-                        <option value="technical">Tech Department</option>
-                        <option value="pr">Public Relations</option>
-                        <option value="support">General Support</option>
+                    <select id="recipient" name="recipient" tabindex="5" class="selmenu">
+                        <option value="nhtan">Nguyễn Hữu Tân</option>
+                        <br style="clear:both;">
+                        <option value="ndvinh">Ngô Đăng Vinh</option>
                     </select>
                 </section>
 
@@ -70,7 +64,7 @@
 
         <section id="buttons">
             <input type="reset" name="reset" id="resetbtn" class="resetbtn" value="Reset">
-            <input type="submit" name="submit" id="submitbtn" class="submitbtn" tabindex="7" value="Submit this!">
+            <input type="submit" name="submit" id="submitbtn" class="submitbtn" tabindex="7" value="Submit">
             <br style="clear:both;">
         </section>
     </form>
@@ -78,3 +72,4 @@
 
 </body>
 </html>
+
